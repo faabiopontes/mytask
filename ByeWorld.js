0218ad67b1e35a800cc0e513408717f5a21b38ee
Fabio Pontes <faabiopontes@localhost.localdomain>
2017-09-11 04:20:37 -0300

    Initial Commit

---
+++ ByeWorld.js
@@ -0,0 +1,11 @@
+"use strict";
+var Output = /** @class */ (function () {
+    function Output() {
+    }
+    Output.main = function () {
+        console.log('Hello Wsssorld');
+        return 0;
+    };
+    return Output;
+}());
+Output.main();
---
+++ ByeWorld.ts
@@ -0,0 +1,8 @@
+class Output {
+    public static main(): number {
+        console.log('Hello Wsssorld');
+        return 0;
+    }
+}
+
+Output.main();
